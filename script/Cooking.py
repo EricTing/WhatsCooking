4e9918b6dcccb53cafb0b769cc4b8ccc703edbba
yunding <yding8@lsu.edu>
2015-11-25 00:38:21 -0600

    try to rank by cosine similarity
    1. suffers from the curse of dimensionality
    2. "brazilian" food ranks top because it has least types of ingredients
    
    	modified:   script/Cooking.py

--- script/Cooking.py
+++ script/Cooking.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 from math import log10
 from itertools import chain
+from sklearn.metrics import pairwise_distances
 
 
 train_ifn = "../data/train.json"
@@ -39,7 +40,10 @@ for name, group in raw_dset.groupby('cuisine'):
 
 
 def getTf(cuisine, ingredient):
-    return tfs[cuisine][ingredient]
+    if cuisine in tfs and ingredient in tfs[cuisine]:
+        return tfs[cuisine][ingredient]
+    else:
+        return 0.0
 
 # ### calculate inverse document frequence
 cuisines_ingredients = {}  # the ingredients of each cuisine
@@ -50,19 +54,22 @@ for name, group in raw_dset.groupby('cuisine'):
     cuisines_ingredients[name] = set(ingredients)
 
 idf = {}
-
-
+total_cuisines = len(set(raw_dset.cuisine))
 for ingredient in set(all_ingredients):
-    num_cuisines_contains_this_ingredient = len([_ for _ in cuisines_ingredients
-                                                 if ingredient in cuisines_ingredients[_]])
-    idf[ingredient] = log10(len(set(raw_dset.cuisine)) / num_cuisines_contains_this_ingredient)
+    num_cuisines_contains_this_ingredient = float(len([_ for _ in cuisines_ingredients
+                                                       if ingredient in cuisines_ingredients[_]]))
+    idf[ingredient] = log10(total_cuisines / num_cuisines_contains_this_ingredient)
+
+
+def getIdf(ingredient):
+    return idf[ingredient]
 
 
 cuisines = set(raw_dset.cuisine)
 
 
 def calcualteIfIdfOfIngredient(ingredient, cuisine):
-    return idf[ingredient] * tfs[cuisine][ingredient]
+    return getIdf(ingredient) * getTf(cuisine, ingredient)
 
 
 def calculateTfIdfOfCuisine(ingredients, cuisine):
@@ -82,3 +89,52 @@ def rankByTfIdf(ingredients):
 predicted_cuisines = raw_dset.ingredients.apply(lambda ingredients: rankByTfIdf(ingredients))
 accuracy = sum(predicted_cuisines == raw_dset.cuisine) / float(len(raw_dset.cuisine))
 print accuracy
+
+## vector space model
+## Scoring, term weighting, and the vector space model, Section 6.3.1
+### ranking by the similarity between the query ingredients' vector and the cuisines' vector
+
+available_ingredients = list(set(all_ingredients))
+indices = {ingredient: available_ingredients.index(ingredient)
+           for ingredient in available_ingredients}
+
+def calculateCuisinVectors():
+    """entries are the tf-idf values
+    """
+    cuisine_vectors = {}
+    for cuisine in set(raw_dset.cuisine):
+        my_vector = [0.0] * len(available_ingredients)
+        for ingredient in available_ingredients:
+            my_vector[indices[ingredient]] = calcualteIfIdfOfIngredient(ingredient, cuisine)
+
+        cuisine_vectors[cuisine] = my_vector
+
+    return cuisine_vectors
+
+cuisine_vectors = calculateCuisinVectors()
+
+def calculateVectorIfBelongs2ThisCuisine(cuisine, ingredients):
+    my_vector = [0.0] * len(available_ingredients)
+    for ingredient in available_ingredients:
+        my_vector[indices[ingredient]] = calcualteIfIdfOfIngredient(ingredient, cuisine)
+
+    return my_vector
+
+def calculateSim(v1, v2):
+    return pairwise_distances(v1, v2, metric='cosine')
+
+def rankByVecSim(ingredients):
+    sims = []
+    for cuisine in cuisines:
+        cuisine_vector = cuisine_vectors[cuisine]
+        ingredients_vector = calculateVectorIfBelongs2ThisCuisine(cuisine, ingredients)
+        sim = calculateSim(cuisine_vector, ingredients_vector)
+        sims.append((cuisine, sim))
+    sims.sort(key=lambda t: t[1], reverse=True)
+    return sims[0][0]
+
+test_dset = raw_dset.sample(300)
+predicted_cuisines = test_dset.ingredients.apply(lambda ingredients: rankByVecSim(ingredients))
+accuracy = sum(predicted_cuisines == test_dset.cuisine) / float(len(test_dset.cuisine))
+print accuracy
+
